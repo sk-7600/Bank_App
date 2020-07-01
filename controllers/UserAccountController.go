7c6db7e4925d00a3d6589e5618c51d0a4b23e63c
sk-7600 <65347439+sk-7600@users.noreply.github.com>
2020-07-01 14:12:48 +0530

    Update web folder for request and response

--- controllers/UserAccountController.go
+++ controllers/UserAccountController.go
@@ -1,15 +1,13 @@
 package controllers
 
 import (
-	"encoding/json"
-	"errors"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	uuid "github.com/satori/go.uuid"
 	"github.com/sk-7600/Bank_App/BankApp/model"
 	"github.com/sk-7600/Bank_App/BankApp/service"
+	"github.com/sk-7600/Bank_App/BankApp/web"
 )
 
 type UserAccountController struct {
@@ -33,38 +31,30 @@ func (uac *UserAccountController) RegisterRoutes(router *mux.Router) {
 
 func (uac *UserAccountController) deleteUser(w http.ResponseWriter, r *http.Request) {
 	user := model.User{}
-	err := UnmarshalJSON(r, &user)
+	err := web.UnmarshalJSON(r, &user)
 	if err != nil {
-		var x = []byte(err.Error())
-		w.Write(x)
+		web.WriteErrorInResponse(err, w)
 	} else {
 		var x = []byte("Data Deleted...")
 		w.Write(x)
 	}
 	er := uac.uas.DeleteUserAccount(user)
-	if er != nil {
-		var x = []byte(er.Error())
-		w.Write(x)
-	}
-	RespondJSON(&w, http.StatusOK, user)
+	web.WriteErrorInResponse(er, w)
+	web.RespondJSON(&w, http.StatusOK, user)
 }
 
 func (uac *UserAccountController) updateUser(w http.ResponseWriter, r *http.Request) {
 	user := model.User{}
-	err := UnmarshalJSON(r, &user)
+	err := web.UnmarshalJSON(r, &user)
 	if err != nil {
-		var x = []byte(err.Error())
-		w.Write(x)
+		web.WriteErrorInResponse(err, w)
 	} else {
 		var x = []byte("Data Updated...")
 		w.Write(x)
 	}
 	er := uac.uas.UpdateUserAccount(user)
-	if er != nil {
-		var x = []byte(er.Error())
-		w.Write(x)
-	}
-	RespondJSON(&w, http.StatusOK, user)
+	web.WriteErrorInResponse(er, w)
+	web.RespondJSON(&w, http.StatusOK, user)
 }
 
 func getIdFromRequest(req *http.Request) string {
@@ -79,19 +69,12 @@ func (uac *UserAccountController) DeleteUserByID(w http.ResponseWriter, r *http.
 	user := model.User{}
 	user.ID = userid
 	er1 := uac.uas.GetUserByID(&user)
-	if er1 != nil {
-		var x = []byte(er1.Error())
-		w.Write(x)
-	}
+	web.WriteErrorInResponse(er1, w)
 	er2 := uac.uas.DeleteUserAccount(user)
-	if er2 != nil {
-		var x = []byte(er2.Error())
-		w.Write(x)
-	}
-	err := UnmarshalJSON(r, &user)
+	web.WriteErrorInResponse(er2, w)
+	err := web.UnmarshalJSON(r, &user)
 	if err != nil {
-		var x = []byte(err.Error())
-		w.Write(x)
+		web.WriteErrorInResponse(err, w)
 	} else {
 		var x = []byte("Data Deleted...")
 		w.Write(x)
@@ -101,49 +84,21 @@ func (uac *UserAccountController) DeleteUserByID(w http.ResponseWriter, r *http.
 func (uac *UserAccountController) AllUsers(w http.ResponseWriter, r *http.Request) {
 	content := []model.User{}
 	er := uac.uas.GetAllUsers(&content)
-	if er != nil {
-		var x = []byte(er.Error())
-		w.Write(x)
-	}
+	web.WriteErrorInResponse(er, w)
 	//fmt.Println(content)
-	RespondJSON(&w, http.StatusOK, content)
+	web.RespondJSON(&w, http.StatusOK, content)
 }
 
 func (uac *UserAccountController) NewUser(w http.ResponseWriter, r *http.Request) {
 	user := model.User{}
-	err := UnmarshalJSON(r, &user)
+	err := web.UnmarshalJSON(r, &user)
 	if err != nil {
-		var x = []byte(err.Error())
-		w.Write(x)
+		web.WriteErrorInResponse(err, w)
 	} else {
 		var x = []byte("Data Inserted...")
 		w.Write(x)
 	}
 	er := uac.uas.AddUserAccount(user)
-	if er != nil {
-		var x = []byte(er.Error())
-		w.Write(x)
-	}
+	web.WriteErrorInResponse(er, w)
 
-}
-
-func UnmarshalJSON(r *http.Request, target interface{}) error {
-	if r.Body == nil {
-		return errors.New("There is problem while reading data")
-	}
-
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		return errors.New("Can't handle data")
-	}
-
-	if len(body) == 0 {
-		return errors.New("Empty Data")
-	}
-
-	err = json.Unmarshal(body, target)
-	if err != nil {
-		return errors.New("Unable to Parse Data")
-	}
-	return nil
 }
