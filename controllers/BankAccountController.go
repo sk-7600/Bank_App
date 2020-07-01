8189c1f293ef7712dbbac30ae8c9575c4bc76bf0
sk-7600 <65347439+sk-7600@users.noreply.github.com>
2020-07-01 14:12:04 +0530

    Update web folder for request and response

--- controllers/BankAccountController.go
+++ controllers/BankAccountController.go
@@ -1,12 +1,12 @@
 package controllers
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/sk-7600/Bank_App/BankApp/model"
 	"github.com/sk-7600/Bank_App/BankApp/service"
+	"github.com/sk-7600/Bank_App/BankApp/web"
 )
 
 type BankAccountController struct {
@@ -26,44 +26,21 @@ func (bac *BankAccountController) RegisterRoutes(router *mux.Router) {
 
 func (bac *BankAccountController) NewUserAccount(w http.ResponseWriter, r *http.Request) {
 	bA := model.BankAccount{}
-	err := UnmarshalJSON(r, &bA)
+	err := web.UnmarshalJSON(r, &bA)
 	if err != nil {
-		var x = []byte(err.Error())
-		w.Write(x)
+		web.WriteErrorInResponse(err, w)
 	} else {
 		var x = []byte("Data Inserted...")
 		w.Write(x)
 	}
 	er := bac.bas.AddBankAccount(bA)
-	writeErrorInResponse(er, w)
+	web.WriteErrorInResponse(er, w)
 }
 
 func (bac *BankAccountController) GetAllAcount(w http.ResponseWriter, r *http.Request) {
 	content := []model.BankAccount{}
 	er := bac.bas.GetAllData(&content)
-	writeErrorInResponse(er, w)
+	web.WriteErrorInResponse(er, w)
 	//fmt.Println(content)
-	RespondJSON(&w, http.StatusOK, content)
-}
-
-func RespondJSON(w *http.ResponseWriter, statusCode int, content interface{}) {
-	response, err := json.Marshal(content)
-	if err != nil {
-		writeToHeader(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-	(*w).Header().Set("Content-Type", "application/json")
-	writeToHeader(w, statusCode, response)
-}
-
-func writeToHeader(w *http.ResponseWriter, statusCode int, payload interface{}) {
-	(*w).WriteHeader(statusCode)
-	(*w).Write(payload.([]byte))
-}
-
-func writeErrorInResponse(err error, w http.ResponseWriter) {
-	if err != nil {
-		var x = []byte(err.Error())
-		w.Write(x)
-	}
+	web.RespondJSON(&w, http.StatusOK, content)
 }
